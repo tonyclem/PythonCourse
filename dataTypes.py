7aaddc4c1822859d3d9d4defbaad29d27527f5bf
tonyclem <tonyclem001@gmail.com>
2022-12-16 00:55:37 +0100

    added data types

---
+++ dataTypes.py
@@ -0,0 +1,28 @@
+# In programming, data type is an important concept
+
+## Fundamental Data types
+
+# Variables are containers for storing data values.
+x= 5
+
+y = "clement"
+
+# Text Types:
+str
+ 
+# Numeric Types:
+int 
+float
+complex
+
+# Boolean Types:
+bool
+
+# Sequence Types:
+list 
+tuple
+range
+
+# Set Types:
+set
+frozenset
--- firstPython.py
+++ firstPython.py
@@ -1,6 +1,18 @@
-print("hello world! it clement python first code")
+print("hello world! it clement first python code")
 n = 5
 while n > 0:
     print(n)
     n = n - 1
 print("hello world")
+
+# init and float
+print(type(6)) # int
+print(type(2 -4)) # int
+print(type(2 * 4)) # int
+print(type( 2 / 4)) # 0.5 float
+print(type( 2 ** 3)) # int
+print(type( 5 % 4)) # int
+print( 2 ** 2) # int
+
+print(int("0b101", 2)) # int
+
