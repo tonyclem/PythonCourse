07ae44c2f96f02cf273a2113b0a226c0a1989a5c
tonyclem <tonyclem001@gmail.com>
2022-12-18 21:48:46 +0100

    practice list method and more data types

--- firstPython.py
+++ firstPython.py
@@ -6,13 +6,13 @@ while n > 0:
 # print("hello world")
 
 # init and float
-# print(type(6)) # int
-# print(type(2 -4)) # int
-# print(type(2 * 4)) # int
-# print(type( 2 / 4)) # 0.5 float
-# print(type( 2 ** 3)) # int
-# print(type( 5 % 4)) # int
-# print( 2 ** 2) # int
+print(type(6)) # int
+print(type(2 -4)) # int
+print(type(2 * 4)) # int
+print(type( 2 / 4)) # 0.5 float
+print(type( 2 ** 3)) # int
+print(type( 5 % 4)) # int
+print( 2 ** 2) # int
 
 # formatted strings
 name = "Johnny"
@@ -21,20 +21,20 @@ print('hi {}. you are {} years old'.format(name, age))
 print('hi {}. you are {} years old'.format("Clement", "24"))
 print(f'hi {name}. you are {age} years old')
 
-print('hi {new_name}. you are {age} years old'.format(new_name="sander", age=20))
+# print('hi {new_name}. you are {age} years old'.format(new_name="sander", age=20))
 
 # [start: stop: stepOver]
-# filter_number = '123456'
-# print(filter_number[::2])
+filter_number = '123456'
+print(filter_number[::2])
 
 # birth_year = input('what year were you born')
 
-# now_year_data  = 2022
-# subtract_year = now_year_data - int(birth_year)
+now_year_data  = 2022
+subtract_year = now_year_data - int(birth_year)
 
-# print(f"{birth_year} i'm born on {birth_year}, and my age is {subtract_year}")
+print(f"{birth_year} i'm born on {birth_year}, and my age is {subtract_year}")
 
-# Password converter
+# # Password converter
 user_name = input("user name")
 password = input("password")
 multiple_password = len(password)
@@ -42,3 +42,17 @@ multiple_password = len(password)
 hidden_password = '*' * multiple_password
 
 print(f'{user_name} your password is {hidden_password} is {len(password)} letter long')
+
+# List 
+books = ['the coding', 'art of war', 'habit', "things"]
+books[0] = 'professionally'
+new_cart = books
+new_cart[0] = 'apple'
+print(new_cart)
+print(books)
+print(books[1:3])
+
+
+
+
+
---
+++ listMethods.py
@@ -0,0 +1,31 @@
+# List Methods
+numbers = [4,5,6,1,7]
+numbers.append(100) #append
+print(numbers)
+
+
+box = [2, 4,6,1,7]
+box.extend([50])
+print(box)
+
+#list unpacking 
+a,b,c, * others = [1,2,3,4,5,6,7]
+print(a)
+print(b)
+print(c)
+print(others)
+
+# dictionary
+
+dictionary = {
+    50: [1,4,6],
+    'b': 'hello world',
+    'e': True
+}
+
+print(dictionary )
+
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+
