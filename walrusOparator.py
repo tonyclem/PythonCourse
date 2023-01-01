645b07cfae75331ed734a456bb0e16d1042c1f28
tonyclem <tonyclem001@gmail.com>
2023-01-01 19:03:14 +0100

    last commit

--- walrusOparator.py
+++ walrusOparator.py
@@ -7,3 +7,29 @@ while(( n := len(a)) > 1):
     # print(n)
     a = a[:-1]
 print(a)
+
+# nonlocal variables
+def outer():
+    x = "local"
+    def inner():
+        nonlocal x
+        x = "nonlocal"
+        print("inner:", x)
+    inner()
+    print("outer:", x)
+outer()
+
+# global variables
+total = 0
+def count():
+    global total
+    total += 1
+    return total
+print(count())
+
+total = 0
+def count(total):
+    total += 10
+    return total
+print(count(total))
+
