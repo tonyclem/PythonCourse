fc10ea99aff6abd222860dd3d309b8d0bceb099e
tonyclem <tonyclem001@gmail.com>
2023-01-30 14:34:19 +0100

    still practicing test

---
+++ testing/script.py
@@ -0,0 +1,8 @@
+def do_stuff(num=0):
+    try:
+        if num:
+            return int(num) + 5
+        else:
+            return 'Please provide Number'
+    except ValueError as err:
+        return err
--- testing/test.py
+++ testing/test.py
@@ -1,17 +1,34 @@
 import unittest
-import main
+import script
 
 
 class TestMain(unittest.TestCase):
+    def setUp(self):
+        print('about to test a function')
+
     def test_do_stuff(self):
         test_param = 10
-        result = main.do_stuff(test_param)
+        result = script.do_stuff(test_param)
         self.assertEqual(result, 15)
 
     def test_do_stuff2(self):
         test_param = 'string'
-        result = main.do_stuff(test_param)
-        self.assertTrue(isinstance(result, ValueError))
+        result = script.do_stuff(test_param)
+        self.assertIsInstance(result, ValueError)
+
+    def test_do_stuff3(self):
+        test_param = None
+        result = script.do_stuff(test_param)
+        self.assertEqual(result, 'Please provide Number')
+
+    def test_do_stuff4(self):
+        test_param = ''
+        result = script.do_stuff(test_param)
+        self.assertEqual(result, 'Please provide Number')
+
+    def tearDown(self):
+        print('cleaning up test')
 
 
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
