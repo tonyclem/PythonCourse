f57b61647c506f8789b834fa0083c91947ca6884
tonyclem <tonyclem001@gmail.com>
2023-02-04 19:52:53 +0100

    testing practice

--- testing/game.py
+++ testing/game.py
@@ -8,17 +8,19 @@ def run_guess(guess, answer):
             return True
     else:
         print('hey bozo, i said 1 ~ 10')
+        return False
 
 
 # generate a random number 1 ~ 10
-answer = randint(int(1), int(20))
-# input from user?
-# check that input is a number 1 ~ 10
-while True:
-    try:
-        guess = int(input('guess a number 1 ~ 10: '))
-        run_guess(guess, answer)
-    except ValueError:
-        print('enter a number from 1 to 10')
-        break
-# check if number is the right guess. otherwise
+if __name__ == '__main__':
+    answer = randint(int(1), int(20))
+    # input from user?
+    # check that input is a number 1 ~ 10
+    while True:
+        try:
+            guess = int(input('guess a number 1 ~ 10: '))
+            run_guess(guess, answer)
+        except ValueError:
+            print('enter a number from 1 to 10')
+            break
+    # check if number is the right guess. otherwise
--- testing/test.py
+++ testing/test.py
@@ -33,7 +33,22 @@ class TestMain(unittest.TestCase):
 
 class TestGame(unittest.TestCase):
     def test_input(self):
-        pass
+        answer = 5
+        guess = 5
+        result = game.run_guess(answer, guess)
+        self.assertTrue(result)
+
+    def test_input_wrong_guess(self):
+        result = game.run_guess(5, 0)
+        self.assertFalse(result)
+
+    def test_input_wrong_number(self):
+        result = game.run_guess(5, 11)
+        self.assertFalse(result)
+
+    def test_input_wrong_type(self):
+        result = game.run_guess(5, 11)
+        self.assertFalse(result)
 
 
 if __name__ == '__main__':
